aa88016a155e9e2b3d2d28ce617e7a77476eafe2
ananarcisio <anaelisafernandesn06@email.com>
2025-09-05 18:37:36 -0300

    atualizando as pastas

--- src/main/java/com/ananarcisio/netflix/controller/HomeController.java
+++ src/main/java/com/ananarcisio/netflix/controller/HomeController.java
@@ -1,5 +1,5 @@
 package com.ananarcisio.netflix.controller;
-
+//colocar um 'decotion' da controller aqui em java
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
